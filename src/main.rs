f08118c3cd88825adb73fdf4ff5d28e6f8bea7c5
sidju <contact@sidju.se>
2020-03-28 23:19:51 +0100

    Now reads files given as args into buffer on startup.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ pub struct State{
     // Configurations
     selection: Option<(usize, usize)>, // The start and end of selected lines
     prompt: Option<String>, // The string printed out when accepting commands
+    file: Option<String>,// The one to write to by default
     // state variables
     done: bool, // Marks that it is time to exit
     error: Option<String>, // Tracks the latest error
@@ -20,6 +21,7 @@ impl State {
         Self {
             selection: None,
             prompt: None,
+            file: None,
             done: false,
             error: None,
             stdin: std::io::stdin(),
@@ -29,16 +31,45 @@ impl State {
 }
 
 fn main() {
-    // TODO: take command line input, such as filename
-
     println!("Welcome to red, the rust-ed.");
     //println!("For assistance, enter '?'.");
+
+    // Init command string
+    let mut command = String::new();
 
     // Init state
     let mut state = State::new();
 
-    // Store command strings separately
-    let mut command = String::new();
+    // Parse command line args
+    let mut i = 0;
+    for arg in std::env::args() {
+        if i != 0 {
+            match arg {
+                filename => {
+                    use std::io::{BufRead, BufReader};
+                    // Open the file TODO, better error handling
+                    let file =
+                        std::fs::OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(true) // If the file is not found it is created
+                        .open(filename)
+                        .unwrap();
+                    // A buffered reader is required to read line by line
+                    let mut reader = BufReader::new(file);
+                    // Loop reading the lines of the file into the buffer.
+                    loop {
+                        let mut line = String::new();
+                        match reader.read_line(&mut line).unwrap() {
+                            0 => break, // Means we have reached EOF
+                            _ => state.buffer.push(line),
+                        }
+                    }
+                },
+            }
+        }
+        i +=1;
+    }
 
     // Loop until done. Take, identify and execute commands
     while !state.done {
